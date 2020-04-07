17e51652e7816167c1142da092f6b6ae723c034b
emilianzugaj <46485041+emilianzugaj@users.noreply.github.com>
2020-04-07 10:49:41 +0200

    Initial commit

---
+++ black jack.py
@@ -0,0 +1,40 @@
+import random
+deck=[]
+
+for i in range(2,11):
+    for a in range(4):
+        deck.append(str(i))
+for a in range(4):
+    deck.append("jack")
+    deck.append("queen")
+    deck.append("king")
+    deck.append("As")
+
+    def new_deck
+
+    def add_card_from_deck(deck,hand):
+        if deck ==[]:
+            new_deck()
+
+
+random.shuffle(deck)
+print(deck)
+
+dealer_hand=[]
+player_hand=[]
+
+dealer_hand.append(deck.pop)
+print("dealer card hiden")
+a=deck.pop()
+dealer_hand.append(a)
+print("dealer got",dealer_hand[1])
+a=deck.pop()
+player_hand.append(a)
+print("you got",player_hand[0])
+a=deck.pop()
+player_hand.append(a)
+print("you got",player_hand[1])
+
+
+
+
